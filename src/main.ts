913141b24a41fdc41b59245a2c67254945bb52be
Alex Dennis <alex.dennis@gmail.com>
2023-05-28 22:06:52 -0700

    refactor: Introduce chains, prompts and commander
    - Add LangChain chains and prompts to leverage the framework abstractions
    - Add Commander to make it easier to manage CLI input

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,66 @@
-const { OpenAI } = require("langchain/llms/openai");
+const {
+  ChatPromptTemplate,
+  HumanMessagePromptTemplate,
+  SystemMessagePromptTemplate,
+} = require("langchain/prompts");
+const { LLMChain } = require("langchain/chains");
+const { ChatOpenAI } = require("langchain/chat_models/openai");
+const { program } = require("commander");
 
 // Import the .env file
 require("dotenv").config();
 
-// Initialize the model
-const model = new OpenAI({
-  temperature: 0.8,
-});
+// Define our program
+program
+  .name("book-chatbot")
+  .description("CLI to talk to characters in books")
+  .version("alpha");
 
-// Define the prompt
-const character = "Alice";
-const book = "The Adventures of Alice in Wonderland";
-const question = "Why did you follow the white rabbit?";
+// Define the chat command
+program
+  .command("chat")
+  .description("Chat with a character in a book")
+  .argument("<book>", "Book name")
+  .argument("<character>", "Book character")
+  .argument("<question>", "Question to ask")
+  .action(async (book: string, character: string, question: string) => {
+    console.info({ book, character, question });
+    const llmChain = createChain();
+    const res = await llmChain.call({
+      character,
+      book,
+      question,
+    });
+    console.info({ res });
 
-const PROMPT = `
-You are a chatbot designed to give users the ability to talk to characters in any book. Play the role of ${character} in the ${book}.  
+    if (res.error) {
+      console.error(res.error);
+    } else if (res.text) {
+      console.log(res.text);
+    }
+  });
+
+// Parse CLI input
+program.parse();
+
+/**
+ * Chreate a new LLMChain
+ *
+ * @param temperature Default 0 to give deterministic results
+ * @returns
+ */
+function createChain(temperature: number = 0) {
+  const chat = new ChatOpenAI({ temperature });
+  const chatPrompt = ChatPromptTemplate.fromPromptMessages([
+    SystemMessagePromptTemplate.fromTemplate(
+      `You are a chatbot designed to give users the ability to talk to characters in any book. Play the role of {character} in the {book}.  
 Don't break out of character. Please give chapter and verse numbers that validate your answers where possible. 
-Respond in less than 500 words and avoid repetition.
-
-question: ${question}
-answer:`;
-
-// Define the main function
-const main = async () => {
-  const res = await model.call(PROMPT);
-  console.log({ res });
-};
-
-// Run the main function
-main();
+Respond in less than 500 words and avoid repetition.`
+    ),
+    HumanMessagePromptTemplate.fromTemplate("{question}"),
+  ]);
+  return new LLMChain({
+    prompt: chatPrompt,
+    llm: chat,
+  });
+}
